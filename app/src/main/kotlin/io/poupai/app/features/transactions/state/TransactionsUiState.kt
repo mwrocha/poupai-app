2c7e84b6aa21589cbc1c0fe09fc58396ad1209ec
Mayke <mwrrestudos@gmail.com>
2026-04-17 06:16:06 -0300

    feat: initial implementation of Poupaí app - auth flow and backend

--- app/src/main/kotlin/io/poupai/app/features/transactions/state/TransactionsUiState.kt
+++ app/src/main/kotlin/io/poupai/app/features/transactions/state/TransactionsUiState.kt
@@ -24,7 +24,7 @@ data class TransactionsUiState(
     val isFormValid: Boolean
         get() = formTitle.isNotBlank() &&
                 formAmount.isNotBlank() &&
-                formAmount.toDoubleOrNull() != null &&
+                formAmount.replace(",", ".").toDoubleOrNull() != null &&
                 formCategory.isNotBlank() &&
                 formDate.isNotBlank()
 }
